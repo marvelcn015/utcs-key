569ee44f129af02dcd07ff0fd72c9f063ac728d2
Aaron Yeh <137107162+marvelcn015@users.noreply.github.com>
2025-01-15 18:44:08 +0800

    chore: change `bootstrap` install method
    change `bootstrap` install method from pnpm to CDN, preventing the deprecated sass `@import` method warnings.

--- src/main.js
+++ src/main.js
@@ -4,9 +4,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/styles/main.scss'
-// import * as bootstrap from 'bootstrap'
-
 const app = createApp(App)
 
 app.use(createPinia())
