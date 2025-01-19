68a9daf491b76534d92ce9e2894dfc63a071de64
kevinhuang09 <146000348+kevinhuang09@users.noreply.github.com>
2025-01-19 10:12:43 +0800

    feat:format and modification
    I try to use prettier to verify format , and revise the <a> in the HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,34 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
 
 <template>
   <nav class="navbar navbar-expand-md bg-dark navbar-dark">
-    <a class="navbar-brand" href="./">UTCS 物品借用系統</a>
-    
+    <RouterLink class="navbar-brand">UTCS 物品借用系統</RouterLink>
+
     <!-- 5.3.1(data-toggle) -> 5.3.3(data-bs-toggle) -->
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#collapsibleNavbar"
+    >
       <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="collapsibleNavbar">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="./borrow.html">借用 Borrow</a>
+          <RouterLink class="nav-link">借用 Borrow</RouterLink>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="./return.html">歸還 Return</a>
+          <RouterLink class="nav-link">歸還 Return</RouterLink>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="./status.php">狀態 Status</a>
+          <RouterLink class="nav-link">狀態 Status</RouterLink>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="./log.php">紀錄 Log</a>
+          <RouterLink class="nav-link">紀錄 Log</RouterLink>
         </li>
         <li class="nav-item dropdown">
           <a
@@ -35,20 +42,23 @@
             帳戶 Account
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="./showapplicationsheet.html">系統使用申請書 Application Sheet (限系外訪客)</a>
-            <a class="dropdown-item" href="./manageaccount.html">管理 Admin</a>
+            <RouterLink class="dropdown-item"
+              >系統使用申請書 Application Sheet (限系外訪客)</RouterLink
+            >
+            <RouterLink class="dropdown-item">管理 Admin</RouterLink>
           </div>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="./manageproperty.html">物品管理 Property Management</a>
+          <RouterLink class="nav-link">物品管理 Property Management</RouterLink>
         </li>
         <li class="nav-item">
-          <a class="nav-link" data-bs-toggle="modal" data-bs-target="#ContactModal">聯絡 Contact</a>
+          <RouterLink class="nav-link" data-bs-toggle="modal" data-bs-target="#ContactModal"
+            >聯絡 Contact</RouterLink
+          >
         </li>
       </ul>
     </div>
   </nav>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
