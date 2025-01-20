c42b708659e34b6c71858db26bc3dfb46e68ed97
ariel511 <ariel.campanula@gmail.com>
2025-01-20 15:52:11 +0800

    feat: modification of info card

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,24 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
 
 <template>
-  <div class="container"></div>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="card text-center">
+        <h4 class="card-title pt-3">info</h4>
+        <div class="card-body">
+          <h5 class="lh-base">
+            This is the Property Management System of
+            <a href="http://cs.utaipei.edu.tw"> UTCS</a>.<br />
+            Please follow the instructions from the staff in the office and give the correct
+            information.<br />
+            使用時請遵守系辦公室的規範。
+          </h5>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
