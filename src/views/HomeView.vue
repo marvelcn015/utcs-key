b6c4e3a0af0d6220e6acbd2d7986c1f91ce1cbc2
Aaron Yeh <137107162+marvelcn015@users.noreply.github.com>
2025-02-03 19:02:29 +0800

    chore: meet requirements for merge

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,34 @@
 <script setup>
-import { RouterLink } from 'vue-router'
+import NavigationBar from '@/components/NavigationBar.vue'
 </script>
 
 <template>
-  <div class="container mt-5">
-    <div class="row">
-      <div class="card text-center">
-        <h4 class="card-title pt-3">info</h4>
-        <div class="card-body">
-          <h5 class="lh-base">
-            This is the Property Management System of
-            <a href="http://cs.utaipei.edu.tw"> UTCS</a>.<br />
-            Please follow the instructions from the staff in the office and give the correct
-            information.<br />
-            使用時請遵守系辦公室的規範。
-          </h5>
+  <NavigationBar />
+  <div class="container px-4">
+    <div
+      class="bg-white rounded-xl shadow-lg overflow-hidden transform transition-all hover:shadow-xl pt-5"
+    >
+      <div class="p-6 md:p-8">
+        <div class="text-center space-y-4">
+          <h3 class="text-xl font-semibold text-gray-800">Welcome to UTCS Key Management System</h3>
+
+          <p class="text-gray-600 leading-relaxed">
+            This is the Key Management System of
+            <a
+              href="http://cs.utaipei.edu.tw"
+              class="text-blue-600 hover:text-blue-700 font-medium transition-colors"
+            >
+              UTCS
+            </a>
+          </p>
+
+          <div class="bg-blue-50 rounded-lg p-4 mt-4">
+            <p class="text-gray-700 leading-relaxed">
+              Please follow the instructions from the staff in the office and provide the correct
+              information.
+            </p>
+            <p class="text-gray-700 mt-2 font-medium">使用時請遵守系辦公室的規範。</p>
+          </div>
         </div>
       </div>
     </div>
